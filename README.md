da13d9e78cc2666734c62424abd24e4a0c823bf6
Kateryna <ikatya7575@gmaiil.com>
2023-11-14 15:51:20 +0300

    made HTML, add CSS and JavaScript code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hi_nails_studio
+
+Beauty studio lottery app where you can win a gift from the studio.
+Technologies used: HTML, CSS, JavaScript
+![Alt text](image.png)
+![Alt text](image-1.png)
+![Alt text](image-2.png)
---
+++ js/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    gift: "-5%",
+    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "-15%",
+    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "-10% ",
+    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "Масло для кутикули",
+    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "Крем для рук",
+    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "Масло для кутикули",
+    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "Крем для рук",
+    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+  },
+  {
+    gift: "Смаколик",
+    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+  },
+];
