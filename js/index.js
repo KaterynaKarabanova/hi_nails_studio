5316c32da8f191848df6ee848e54c06df06d58aa
Kateryna <ikatya7575@gmaiil.com>
2023-11-14 16:14:22 +0300

    added refs.js, cleared styles

--- js/index.js
+++ js/index.js
@@ -1,25 +1,18 @@
 import { data } from "./data.js";
+import { refs } from "./refs.js";
 
-console.log(data);
-const logoSection = document.querySelector(".bg");
-const circle = document.querySelector(".circle-img");
-const rotateBtn = document.querySelector(".btn-5");
-const circleWrapper = document.querySelector(".container-circle");
-const gift = document.querySelector(".modal-gift");
-const modal = document.querySelector(".modal");
-const modalText = document.querySelector(".modal-text");
 setTimeout(() => {
-  logoSection.classList.add("none");
+  refs.logoSection.classList.add("none");
 }, 1600);
 
-rotateBtn.addEventListener("click", onDanceBtnClick);
+refs.rotateBtn.addEventListener("click", onDanceBtnClick);
 
 function onDanceBtnClick() {
   rotateCircle();
 }
 
 function rotateCircle() {
-  circle.classList.add("rotating");
+  refs.circle.classList.add("rotating");
   setTimeout(() => {
     getSong();
   }, 5000);
@@ -28,17 +21,17 @@ function rotateCircle() {
 function getSong() {
   const giftIndex = Math.floor(Math.random() * 8);
   const giftObj = data[Number(giftIndex)];
-  circleWrapper.classList.add("hidden");
-  modal.classList.remove("hidden");
-  modal.style.opacity = "100";
+  refs.circleWrapper.classList.add("hidden");
+  refs.modal.classList.remove("hidden");
+  refs.modal.style.opacity = "100";
   if (giftIndex > 2) {
-    gift.style.fontSize = "34px";
-    gift.style.top = "30%";
+    refs.gift.style.fontSize = "34px";
+    refs.gift.style.top = "30%";
   }
   if (giftIndex === 3 || giftIndex === 5) {
-    gift.style.fontSize = "34px";
-    gift.style.top = "15%";
+    refs.gift.style.fontSize = "34px";
+    refs.gift.style.top = "15%";
   }
-  gift.textContent = `${giftObj.gift}`;
-  modalText.textContent = `${giftObj.description}`;
+  refs.gift.textContent = `${giftObj.gift}`;
+  refs.modalText.textContent = `${giftObj.description}`;
 }
---
+++ js/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+  logoSection: document.querySelector(".bg"),
+  circle: document.querySelector(".circle-img"),
+  rotateBtn: document.querySelector(".btn-5"),
+  circleWrapper: document.querySelector(".container-circle"),
+  gift: document.querySelector(".modal-gift"),
+  modal: document.querySelector(".modal"),
+  modalText: document.querySelector(".modal-text"),
+};
