d2baf198a758abc91eb64df3c93f1a6f3ab2654a
Kateryna <ikatya7575@gmaiil.com>
2023-11-16 23:29:52 +0300

    changed data, corrected transitions

--- js/data.js
+++ js/data.js
@@ -1,34 +1,62 @@
 export const data = [
   {
     gift: "-5%",
-    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+    description: "* Знижка діє на будь-яку процедуру салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "-5%",
+    description: "* Знижка діє на будь-яку процедуру салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "-5%",
+    description: "* Знижка діє на будь-яку процедуру салону hi.nails.kyiv. ",
   },
   {
     gift: "-15%",
-    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+    description: "* Знижка діє на будь-яку процедуру салону hi.nails.kyiv. ",
   },
   {
     gift: "-10% ",
-    description: "*Знижка діє на будь-яку процедуру салону hi.nails,kyiv. ",
+    description: "* Знижка діє на будь-яку процедуру салону hi.nails.kyiv. ",
   },
   {
     gift: "Масло для кутикули",
-    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
-  },
-  {
-    gift: "Крем для рук",
-    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
   },
   {
     gift: "Масло для кутикули",
-    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Масло для кутикули",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
   },
   {
     gift: "Крем для рук",
-    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
   },
   {
-    gift: "Смаколик",
-    description: "*Забирай свій подаруночок y майстра  салону hi.nails,kyiv. ",
+    gift: "Крем для рук",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Подарунковий набір",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Догляд для брів",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Догляд для вій",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Крем для рук",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
+  },
+  {
+    gift: "Крем для рук",
+    description: "* Забирай свій подаруночок y майстра  салону hi.nails.kyiv. ",
   },
 ];
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { refs } from "./refs.js";
 
 setTimeout(() => {
   refs.logoSection.classList.add("none");
-}, 1600);
+}, 1450);
 
 refs.rotateBtn.addEventListener("click", onDanceBtnClick);
 
@@ -20,17 +20,24 @@ function rotateCircle() {
 }
 
 function getSong() {
-  const giftIndex = Math.floor(Math.random() * 8);
+  const giftIndex = Math.floor(Math.random() * 15);
   const giftObj = data[Number(giftIndex)];
   refs.circleWrapper.classList.add("hidden");
   refs.modal.classList.remove("hidden");
   refs.modal.style.opacity = "100";
-  if (giftIndex > 2) {
+  if (giftIndex > 5) {
     refs.gift.style.fontSize = "34px";
     refs.gift.style.top = "30%";
   }
-  if (giftIndex === 3 || giftIndex === 5) {
-    refs.gift.style.fontSize = "34px";
+  if (
+    giftIndex === 5 ||
+    giftIndex === 6 ||
+    giftIndex === 7 ||
+    giftIndex === 12 ||
+    giftIndex === 11 ||
+    giftIndex === 10
+  ) {
+    refs.gift.style.fontSize = "33px";
     refs.gift.style.top = "15%";
   }
   refs.gift.textContent = `${giftObj.gift}`;
